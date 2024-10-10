a6b9c17874f7c242b5e4e18268348afe7c71c941
LuisBarquero19 <000838@uca.edu.sv>
2024-10-09 22:40:06 -0600

    Prueba

--- src/components/manejoProyectos.component.js
+++ src/components/manejoProyectos.component.js
@@ -4,7 +4,6 @@ const ManejoProyectos = () => {
     return (
         <div>
             <h2>Manejo de Proyectos</h2>
-            <p>Esta es la página de manejo de proyectos.</p>
         </div>
     );
 };
